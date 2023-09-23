50db55e7237250a67eed9366a009ed8600bc58bd
someone <aidan.a.sullivan@gmail.com>
2023-09-23 16:13:41 -0500

    changes all links to red

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
-var listOfLinks = [];
-var collectLinks = document.links;
-for (var i=0; i<collectLinks.length; i++) {
-  listOfLinks.push(collectLinks[i].href);
+// content.js
+
+// Function to change the color of all links to red
+function changeLinkColorToRed() {
+  const links = document.querySelectorAll('a'); // Select all anchor elements
+
+  // Loop through each link and change its color to red
+  links.forEach(link => {
+     link.style.color = 'red';
+  });
 }
-console.log(listOfLinks);
+
+// Call the function when the page has loaded
+window.addEventListener('load', changeLinkColorToRed);
